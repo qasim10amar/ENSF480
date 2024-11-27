b2167113e7aaebb190bc9b31236cf87936680321
Qasim Amar <qasim10amar@gmail.com>
2024-11-26 22:16:32 -0700

    Test

--- src/main/java/org/ensf480/Boundary/MovieService.java
+++ src/main/java/org/ensf480/Boundary/MovieService.java
@@ -1,7 +1,9 @@
 package org.ensf480.Boundary;
 
 public class MovieService {
-
+    public void createMovie() {
+        System.out.println("Creating movie...");
+    }
 
 
     public void viewMovie() {
