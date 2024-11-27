894c284377c984f4aab040792e1560a0227cc4f5
Hamza Amar <rhamzax@gmail.com>
2024-11-26 19:12:58 -0700

    Test

--- src/main/java/org/ensf480/Boundary/MovieService.java
+++ src/main/java/org/ensf480/Boundary/MovieService.java
@@ -3,9 +3,6 @@ package org.ensf480.Boundary;
 public class MovieService {
 
 
-    public void updateMovie() {
-        System.out.println("Updating movie...");
-    }
 
     public void viewMovie() {
         System.out.println("Viewing movie...");
